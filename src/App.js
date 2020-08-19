4a0e629ac0e2b6c4ab3fdb912aedbcaa9a0bea99
Turkay Tunc <turkaytunc00@gmail.com>
2020-08-19 17:56:48 +0300

    fixed babel error

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import './app.css';
+
 export const App = () => {
+  console.log('hey');
   return (
     <div className="app">
-      <p>Hello from react-app</p>
+      <p>Hello from react-app.</p>
+      <h1>Hello</h1>
     </div>
   );
 };
